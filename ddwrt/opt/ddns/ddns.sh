774d68623b14ab59a1b5677aa7b51097ad32df29
Chad Voegele <cavoegele@gmail.com>
2017-07-16 09:26:21 -0700

    Switch to curl with tlsv1.1. Also use env vars for user/pass.

--- ddwrt/opt/ddns/ddns.sh
+++ ddwrt/opt/ddns/ddns.sh
@@ -7,27 +7,24 @@
 # alfer@fusebin.com
 # cavoegele@gmail.com
 
-### SETTINGS SECTION ###
-UPDATE_INTERVAL=60
-DDNS_DOMAIN=$ENV_DDNS_DOMAIN
-DDNS_USERNAME=$ENV_DDNS_USERNAME
-DDNS_PASSWORD=$ENV_DDNS_PASSWORD
-DDNS_HOST=domains.google.com
+if [ -z $DDNS_DOMAIN ] || [ -z $DDNS_USERNAME ] || [ -z $DDNS_PASSWORD ]; then
+  echo "DDNS_DOMAIN, DDNS_USERNAME, DDNS_PASSWORD must be set"
+  exit 1
+fi
+UPDATE_INTERVAL=${UPDATE_INTERVAL:-60}
+DDNS_HOST=${DDNS_HOST:-domains.google.com}
 
-LOG_FILE=/var/log/dyndns.log
+LOG_FILE=/var/log/ddns.log
 CA_BUNDLE=/opt/ddns/cacert.pem  # https://curl.haxx.se/docs/caextract.html
-### END SETTINGS SECTION
 
-HTTP_AUTH=`echo -n "$DDNS_USERNAME:$DDNS_PASSWORD" | openssl base64 -base64`
-REQUEST="GET /nic/update?hostname=$DDNS_DOMAIN HTTP/1.1\r\nHost: $DDNS_HOST\r\nAuthorization: Basic $HTTP_AUTH\r\nAccept: */*\r\n\r\n"
-OPENSSL_CMD="openssl s_client -connect $DDNS_HOST:443 -CAfile $CA_BUNDLE -no_ticket -tls1_1"
+CURL_CMD="curl --silent --show-error --cacert /opt/ddns/cacert.pem --tlsv1.1 https://$DDNS_USERNAME:$DDNS_PASSWORD@$DDNS_HOST/nic/update?hostname=$DDNS_DOMAIN"
 
 while sleep $UPDATE_INTERVAL
 do
   CURRENT_TIME=`date`
   CURRENT_WAN_IP=`nvram get wan_ipaddr`
   LAST_WAN_IP=`nvram get wan_ipaddr_last`
-  DNS_IP=`nslookup $DDNS_DOMAIN 8.8.8.8 | grep Address | tail -n 1 | awk '{print $3}'`
+  DNS_IP=`nslookup $DDNS_DOMAIN 8.8.8.8 | grep -A 1 $DDNS_DOMAIN | awk 'FNR==2{print $3}'` 2>> $LOG_FILE
 
   # Check if IP Address has changed from last locally stored IP
   if [ -z $LAST_WAN_IP ] || [ $CURRENT_WAN_IP != $LAST_WAN_IP ]; then
@@ -35,19 +32,20 @@ do
     nvram set wan_ipaddr_last=$CURRENT_WAN_IP
     nvram commit
     echo "$CURRENT_TIME: Updating DynDNS Service" >> $LOG_FILE
-    ( (printf "$REQUEST"; sleep 3) | $OPENSSL_CMD ) >> $LOG_FILE 2>&1
-    echo -e "\n" >> $LOG_FILE
+    (echo "$CURRENT_TIME: Running $CURL_CMD" | sed "s/$DDNS_PASSWORD/...ELIDED.../") >> $LOG_FILE
+    $CURL_CMD 1>> $LOG_FILE 2>&1
+    echo "" >> $LOG_FILE
 
   # Check if IP Address has changed from what DNS is reporting
-  elif [ $CURRENT_WAN_IP != $DNS_IP ]; then
+  elif [ -z $DNS_IP ] || [ $CURRENT_WAN_IP != $DNS_IP ]; then
     echo "$CURRENT_TIME: DNS IP not up to date, currently: $DNS_IP, but our IP is $CURRENT_WAN_IP" >> $LOG_FILE
     echo "$CURRENT_TIME: Updating DynDNS Service" >> $LOG_FILE
-    ( (printf "$REQUEST"; sleep 3) | $OPENSSL_CMD ) >> $LOG_FILE 2>&1
-    echo -e "\n" >> $LOG_FILE
+    (echo "$CURRENT_TIME: Running $CURL_CMD" | sed "s/$DDNS_PASSWORD/...ELIDED.../") >> $LOG_FILE
+    $CURL_CMD 1>> $LOG_FILE 2>&1
+    echo "" >> $LOG_FILE
 
   else
     echo "$CURRENT_TIME: DNS IP up to date!" >> $LOG_FILE
-    echo -e "\n" >> $LOG_FILE
   fi
 
 done
